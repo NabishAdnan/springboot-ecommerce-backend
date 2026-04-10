720c9bb646eefafc64d7e64e77263812e17fa16b
adnan0112233 <faiyaznabish@gmail.com>
2026-04-10 16:36:02 +0530

    addressdto updated

--- src/main/java/com/backend/e_commerce/DTO/AddressDTO.java
+++ src/main/java/com/backend/e_commerce/DTO/AddressDTO.java
@@ -5,6 +5,7 @@ public class AddressDTO {
 	private long addressId;
     private String addressLine1;
     private String addressLine2;
+    private String addressLine3;
     private String city;
     private String state;
     private String pincode;
